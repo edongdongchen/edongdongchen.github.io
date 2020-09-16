df62377d5a231465351b931f31c1f9c6ad50e6c0
Dongdong Chen <43349309+edongdongchen@users.noreply.github.com>
2020-09-16 14:56:03 +0100

    Add files via upload

---
+++ Dongdong's Homepage_files/dynamic.js
@@ -0,0 +1,10 @@
+_clustrmaps.layer('recent',{'points':[[32.0617,118.7778,"CN","Nanjing"]],timestamp:'201409180043'});
+_clustrmaps.layer('recent',{'points':[[31.8639,117.2808,"CN","Hefei"]],timestamp:'201409170824'});
+_clustrmaps.layer('recent',{'points':[[22.2833,114.1500,"HK","Central District"]],timestamp:'201409170651'});
+_clustrmaps.layer('recent',{'points':[[20.0000,77.0000,"IN","India"]],timestamp:'201409161452'});
+_clustrmaps.layer('recent',{'points':[[39.9289,116.3883,"CN","Beijing"]],timestamp:'201409161053'});
+_clustrmaps.layer('recent',{'points':[[39.4899,-74.4773,"US","Absecon, New Jersey"]],timestamp:'201409160559'});
+_clustrmaps.layer('recent',{'points':[[35.0000,105.0000,"CN","China"],[20.0000,77.0000,"IN","India"]],timestamp:'201409140451'});
+_clustrmaps.layer('recent',{'points':[[35.6900,139.6900,"JP","Japan"]],timestamp:'201409131023'});
+_clustrmaps.layer('recent',{'points':[[34.2005,-118.1362,"US","Altadena, California"]],timestamp:'201409130400'});
+_clustrmaps.layer('recent',{'points':[[25.7651,-80.3603,"US","Miami, Florida"]],timestamp:'201409120654'});
